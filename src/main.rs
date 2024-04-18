e4108e756aaf4d3f2c482541bba0755e6ebf87ab
Hao Hu <huhao@outlook.com>
2024-04-18 21:06:13 +0800

    Implement Player struct and associated functionality
    Implemented a Player struct in new player.rs file that includes methods for rendering and updating player state. Updated main.rs to include the Player struct in the State struct and call render and update methods within the game state tick function.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![warn(clippy::all, clippy::pedantic)]
 
 mod map;
+mod player;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
@@ -9,24 +10,33 @@ mod prelude {
     pub const SCREEN_HEIGHT: i32 = 50;
 
     pub use crate::map::*;
+    pub use crate::player::*;
 }
 
 use prelude::*;
 
 struct State {
     map: Map,
+    player: Player,
 }
 
 impl State {
     fn new() -> Self {
-        Self { map: Map::new() }
+        Self {
+            map: Map::new(),
+            player: Player::new(
+                Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+            ),
+        }
     }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
+        self.player.update(ctx, &self.map);
         self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
 
