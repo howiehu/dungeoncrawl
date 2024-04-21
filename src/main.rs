8959de63e25ca5de9ee3422ee2356c0ca2e71407
Hao Hu <huhao@outlook.com>
2024-04-21 15:23:32 +0800

    Add new MapBuilder struct and update main.rs
    A new struct, MapBuilder, has been created to hold and manage map building logic. Along with various helper methods, this struct allows the creation of multiple rooms randomly placed on the map with passageway between them. The main.rs file has been updated accordingly, to ensure the map is generated through MapBuilder and the player's initial position is set properly.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 
 mod map;
 mod player;
+mod map_builder;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
@@ -11,6 +12,7 @@ mod prelude {
 
     pub use crate::map::*;
     pub use crate::player::*;
+    pub use crate::map_builder::*;
 }
 
 use prelude::*;
@@ -22,11 +24,11 @@ struct State {
 
 impl State {
     fn new() -> Self {
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
         Self {
-            map: Map::new(),
-            player: Player::new(
-                Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-            ),
+            map: map_builder.map,
+            player: Player::new(map_builder.player_start),
         }
     }
 }
