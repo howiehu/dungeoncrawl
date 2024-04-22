741b20de9a1ff233b8db750cccbd3c850efcdcf7
Hao Hu <huhao@outlook.com>
2024-04-22 15:12:19 +0800

    Replace collision system with improved combat system
    The previous collision mechanic has been removed and supplanted with a refined combat mechanism. Enhanced movement logic now enables attack potential checks on destination tiles. Additionally, the player input system has been updated to issue attack requests rather than merely permitting movement into enemy spaces. This shift introduces a more strategic approach to movement and combat within the game.

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -18,17 +18,36 @@ pub fn player_input(
             VirtualKeyCode::Down => Point::new(0, 1),
             _ => Point::new(0, 0),
         };
+        let (player_entity, destination) = players
+            .iter(ecs)
+            .map(|(entity, pos)| (*entity, *pos + delta))
+            .next()
+            .unwrap();
+        let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
         if delta.x != 0 || delta.y != 0 {
-            players.iter_mut(ecs).for_each(|(entity, pos)| {
-                let destination = *pos + delta;
+            let mut hit_something = false;
+            enemies
+                .iter(ecs)
+                .filter(|(_, pos)| **pos == destination)
+                .for_each(|(entity, _)| {
+                    hit_something = true;
+                    commands.push((
+                        (),
+                        WantsToAttack {
+                            attacker: player_entity,
+                            victim: *entity,
+                        },
+                    ));
+                });
+            if !hit_something {
                 commands.push((
                     (),
                     WantsToMove {
-                        entity: *entity,
+                        entity: player_entity,
                         destination,
                     },
                 ));
-            });
+            };
             *turn_state = TurnState::PlayerTurn;
         }
     }
